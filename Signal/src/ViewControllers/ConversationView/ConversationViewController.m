d72a6ce06a24c73565bf782e2fd9f798ac0a02d8
Matthew Chen <charlesmchen@gmail.com>
2017-11-08 15:23:48 -0500

    Fixes for scrolling in conversation view.

--- Signal/src/ViewControllers/ConversationView/ConversationViewController.m
+++ Signal/src/ViewControllers/ConversationView/ConversationViewController.m
@@ -3505,13 +3505,6 @@ typedef NS_ENUM(NSInteger, MessagesRangeSizeMode) {
 
 #pragma mark - ConversationInputTextViewDelegate
 
-- (void)inputTextViewDidBecomeFirstResponder
-{
-    OWSAssert([NSThread isMainThread]);
-
-    [self scrollToBottomAnimated:YES];
-}
-
 - (void)inputTextViewSendMessagePressed
 {
     [self sendButtonPressed];
@@ -3630,7 +3623,7 @@ typedef NS_ENUM(NSInteger, MessagesRangeSizeMode) {
     [self updateLastVisibleTimestamp];
     [self autoLoadMoreIfNecessary];
 
-    if ([self isScrolledAwayFromBottom]) {
+    if (self.isUserScrolling && [self isScrolledAwayFromBottom]) {
         [self.inputToolbar endEditingTextMessage];
     }
 }
